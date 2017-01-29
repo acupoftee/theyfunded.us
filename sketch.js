684536356ef599c2180559ccd80f354bde0a86f7
bethy <bethyd123@gmail.com>
2017-01-28 23:11:52 -0500

    finished

--- sketch.js
+++ sketch.js
@@ -1,66 +1,65 @@
+var t;
 var data;
 var entries = [];
 var n;
 var r, g, b;
 var l = 0;
 
-// get the data. published by the NEA
-function prepList() {
-  var url = 'https://spreadsheets.google.com/feeds/list/1-tM404sCGoyRQYeUCge1qFpfRLt92lfuyYAA0ESN2O8/od6/public/values?alt=json';
-  data = loadJSON(url);
-  document.body.style.backgroundColor = 'white';
+function preload() {
+    var url = 'https://spreadsheets.google.com/feeds/list/1-tM404sCGoyRQYeUCge1qFpfRLt92lfuyYAA0ESN2O8/od6/public/values?alt=json';
+    data = loadJSON(url);
+    document.body.style.backgroundColor = 'white';
 }
 
-// p5.js setup code
+// p5 default function
 function setup() {
-  getLines();
-  noCanvas();
+    getLines();
+    noCanvas();
 }
-
-// p5.js drawing code
-function draw() {}
 
 function shuffle(array) {
-  var current = array.length, tempValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
-  // while we're still shuffling elements
-  while (0 !== current) {
-    // pick remaining element
-    randomIndex = Math.floor(Math.random() * current);
-    current -= 1;
+    // While there remain elements to shuffle
+    while (0 !== currentIndex) {
 
-    // swap with current element
-    tempValue = array[current];
-    array[current] = array[randomIndex];
-    array[randomIndex] = tempValue;
-  }
-  return array;
+        // Pick a remaining element
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
 
-// look through the spreadsheets feed for list items
 function getLines() {
-  for (var i = 0; i < data.feed.entry.length; i++) {
-    n = data.feed.entry.length;
-    var line = data.feed.entry[i].title.$t + " in " + data.feed.entry[i].gsx$_cokwr.$t + ", " + data.feed.entry[i].gsx$_cpzh4.$t + ".";
-    console.log(line);
-    entries.push(line);
-  }
-  entries = shuffle(entries);
-  emitEntries();
+    for (var i = 0; i < data.feed.entry.length; i++) {
+        n = data.feed.entry.length;
+        var line = data.feed.entry[i].title.$t + " in " + data.feed.entry[i].gsx$_cokwr.$t + ", " + data.feed.entry[i].gsx$_cpzh4.$t + ".";
+        console.log(line);
+        entries.push(line);
+    }
+
+    entries = shuffle(entries);
+    start();
 }
 
-// shove it in their faces with splashes of color
-function emitEntries() {
-  setInterval(function() {
-    r = int(random(255));
-    g = int(random(255));
-    b = int(random(255));
-    var p = document.createElement('p');
-    p.style.color = 'rgb(' + r + ',' + g + ',' + b + ')';
-    p.textContent = entries[l];
-    var items = document.getElementById('items');
-    items.insertBefore(p, items.firstChild);
-    l++;
-    if (l > entries.length - 1) l = 0;
-  }, 1200);
+function start() {
+    setInterval(function() {
+        r = int(random(255));
+        g = int(random(255));
+        b = int(random(255));
+        var p = document.createElement('p');
+        p.style.color = 'rgb(' + r + ',' + g + ',' + b + ')';
+        p.textContent = entries[l];
+        var items = document.getElementById('items');
+        items.insertBefore(p, items.firstChild);
+        l++;
+        if (l > entries.length - 1) l = 0;
+    }, 1200);
 }
